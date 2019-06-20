ff4c4cf3813bec87341d4bc702ec6227adc10c56
mchardex <bukunmiadebisi@gmail.com>
2019-06-20 23:05:49 +0100

    added styling

--- public/chat.js
+++ public/chat.js
@@ -5,11 +5,12 @@ const socket = io.connect(['http://localhost:8000', 'http://66f04576.ngrok.io', 
 const output = document.getElementById('output');
 const handle = document.getElementById('handle');
 const message = document.getElementById('message');
-const send  = document.getElementById('send');
+const form  = document.getElementById('form');
 const feedback  = document.getElementById('feedback');
 
 //  emit event
-send.addEventListener('click', () => {
+form.addEventListener('submit', e => {
+  e.preventDefault();
   socket.emit('chat', {
     message: message.value,
     handle: handle.value
@@ -22,10 +23,10 @@ message.addEventListener('keypress', () => {
 
 // listen for events
 socket.on('chat', (data) => {
-  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+  output.innerHTML += '<p id="output"><strong>' + data.handle + ': </strong>' + data.message + '</p>';
   feedback.innerHTML = '';
 })
 
 socket.on('typing', (data) => {
-  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+  feedback.innerHTML = '<p id="typing"><em>' + data + ' is typing a message...</em></p>';
 })
